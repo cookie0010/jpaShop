217c66b563d82826a684cc49afb5982a42cb3e6d
cookie0010 <sg.been2017@gmail.com>
2023-01-27 12:26:32 +0900

    구현 준비

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+}
